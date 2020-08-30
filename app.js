2a1c8d41e2274f3310218aa91a61909beb158f5a
Duc Nguyen <ducnm@zigvy.com>
2020-08-30 22:11:48 +0700

    commit delete package-log.json add bodypaser mongoose logger

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // Call in installed dependencies
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import logger from 'morgan';
 
-// set up express app
+// set up dependencies
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
 
+// set up mongoose
+mongoose.connect('mongodb://localhost:27017/project_support')
+    .then(() => {
+        console.log('Database connected')
+    })
+    .catch((error) => {
+        console.log('Error connecting to database', error)
+    });
 // set up port number
 const port = 4000;
 
